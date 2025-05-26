5f2c4a98a181d0307658cee1c7874d692683e681
Umang <48994342+UMANG2707@users.noreply.github.com>
2025-05-26 18:37:20 +0530

    Update main.ts

--- Web-App-Angular/src/main.ts
+++ Web-App-Angular/src/main.ts
@@ -1,5 +1,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
